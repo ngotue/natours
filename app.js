d1dc2e92c7982e0b615519d72b3c8569b7486ae9
ngotue <ngodinh.minhtue@gmail.com>
2023-09-11 12:52:31 +0200

    added start script and tested api

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const express =require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World')
+    res.status(200).json({
+        message: 'Hello World',
+        app: 'natours app'
+    })
+})
+
+app.post('/', (req, res) => {
+    res.status(201).send('good job')
 })
 
 const port = 3000
