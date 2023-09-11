888754280d3d9da8c6757576b093c039b6c710ac
unknown <ngodinh.minhtue@gmail.com>
2023-09-11 22:18:29 +0200

    added user routes

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const fs = require('fs');
-
-const app = express();
-
-app.use(express.json()); // this middleware allows us to access req.body
-
-app.use((req, res, next) => {
-  
-})
+const morgan = require('morgan');
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json()); // this middleware allows us to access req.body
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: { tours },
   });
@@ -66,13 +70,14 @@ const updateTour = (req, res) => {
       status: 'Not found',
       message: 'Invalid ID',
     });
+  }else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: 'updated tour',
+      },
+    });
   }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'updated tour',
-    },
-  });
 };
 
 const deleteTour = (req, res) => {
@@ -82,21 +87,72 @@ const deleteTour = (req, res) => {
       status: 'Not found',
       message: 'Invalid ID',
     });
+  }else {
+    res.status(204).json({
+      status: 'success',
+      data: {
+        tour: null,
+      },
+    });
   }
-  res.status(204).json({
-    status: 'success',
-    data: {
-      tour: null,
-    },
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined'
+  })
+}
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined',
   });
 };
 
-app.route('/api/v1/tours').get(getAllTours)
-                          .post(createTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
-app.route('/api/v1/tours/:id').get(getTour)
-                              .patch(updateTour)
-                              .delete(deleteTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
