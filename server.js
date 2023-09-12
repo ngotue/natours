774f52ef619cc5b44a904e94d2f7d0c19fca4603
unknown <ngodinh.minhtue@gmail.com>
2023-09-12 18:58:08 +0200

    added mongodb connection with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 const dotenv = require('dotenv');
 
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const mongoConnectionString = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(mongoConnectionString).then(() => {
+  console.log('DB connection success');
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The park camper',
+  price: 500,
+});
+
+testTour.save();
 
 const port = 3000;
 app.listen(port, () => {
